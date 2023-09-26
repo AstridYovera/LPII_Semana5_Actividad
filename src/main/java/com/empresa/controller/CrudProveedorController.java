85d09d64c0f155d5d8b4f8bc5c297af229fe5cf9
AstridYovera <ayoveratinoco@gmail.com>
2023-09-25 20:30:16 -0500

    Registro Proveedor

---
+++ src/main/java/com/empresa/controller/CrudProveedorController.java
@@ -0,0 +1,89 @@
+package com.empresa.controller;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.empresa.entity.Proveedor;
+import com.empresa.service.ProveedorService;
+
+@Controller
+public class CrudProveedorController {
+
+	@Autowired
+	private ProveedorService proveedorService;
+	
+	@RequestMapping("/verCrudProveedor")
+	public String verInicio() {
+		return "crudProveedor";
+	}
+	
+	@PostMapping("/registraCrudProveedor")
+	@ResponseBody
+	public Map<?, ?> registra(Proveedor obj) {
+		HashMap<String, Object> map = new HashMap<String, Object>();
+		obj.setFechaRegistro(new Date());  
+		Proveedor objSalida = proveedorService.insertaProveedor(obj);
+		if (objSalida == null) {
+			map.put("mensaje", "Error en el registro");
+		} else {
+			List<Proveedor> lista = proveedorService.listaPorNombreLike("%");
+			map.put("lista", lista);
+			map.put("mensaje", "Registro exitoso");
+		}
+		return map;
+	}
+	
+	@PostMapping("/actualizaCrudProveedor")
+	@ResponseBody
+	public Map<?, ?> actualiza(Proveedor obj) {
+		HashMap<String, Object> map = new HashMap<String, Object>();
+		  
+		Optional<Proveedor> optProveedor= proveedorService.buscaProveedor(obj.getIdProveedor());
+		obj.setFechaRegistro(optProveedor.get().getFechaRegistro());
+		Proveedor objSalida = proveedorService.actualizaProveedor(obj);
+		if (objSalida == null) {
+			map.put("mensaje", "Error en actualizar");
+		} else {
+			List<Proveedor> lista = proveedorService.listaPorNombreLike("%");
+			map.put("lista", lista);
+			map.put("mensaje", "Actualización exitosa");
+		}
+		return map;
+	}
+	
+	@ResponseBody
+	@PostMapping("/eliminaCrudProveedor")
+	public Map<?, ?> elimina(int id) {
+		HashMap<String, Object> map = new HashMap<String, Object>();
+		
+		Proveedor objProveedor = proveedorService.buscaProveedor(id).get();
+		Proveedor objSalida = proveedorService.actualizaProveedor(objProveedor);
+
+		if (objSalida == null) {
+			map.put("mensaje", "Error en actualizar");
+		} else {
+			List<Proveedor> lista = proveedorService.listaPorNombreLike("%");
+			map.put("lista", lista);
+		}
+		return map;
+	}
+	
+	@GetMapping("/consultaCrudProveedor")
+	@ResponseBody
+	public List<Proveedor> consulta(String filtro) {
+		return proveedorService.listaPorNombreLike("%"+filtro+"%");
+	}
+	
+	
+	
+}
--- src/main/java/com/empresa/service/ProveedorServiceImpl.java
+++ src/main/java/com/empresa/service/ProveedorServiceImpl.java
@@ -1,5 +1,8 @@
 package com.empresa.service;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,6 +18,26 @@ public class ProveedorServiceImpl implements ProveedorService{
 	@Override
 	public Proveedor insertaProveedor(Proveedor obj) {
 		return repository.save(obj);
+	}
+
+	@Override
+	public List<Proveedor> listaPorNombre(String nombre) {
+		return repository.findByNombreIgnoreCase(nombre);
+	}
+
+	@Override
+	public Proveedor actualizaProveedor(Proveedor obj) {
+		return repository.save(obj);
+	}
+
+	@Override
+	public List<Proveedor> listaPorNombreLike(String nombre) {
+		return repository.findByNombreLike(nombre);
+	}
+
+	@Override
+	public Optional<Proveedor> buscaProveedor(int idProveedor) {
+		return repository.findById(idProveedor);
 	}
 
 }
